2f66faa555ad21789158fc8f622e456016df96d7
bujosa <davidbujosa@gmail.com>
2019-06-05 19:08:41 -0400

    Subida Primera parte de numbers in words

---
+++ CODE KATAS/3. Numbers in Words/CodeKatas/Program.cs
@@ -0,0 +1,60 @@
+﻿
+namespace CodeKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+
+    public class Converted
+    {
+        public string[] waterhouse = { "", "One", "Two", "Three", "Four", "Five", "Six",
+        "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen","Sixteen",
+            "Seventeen", "Eighteen", "Nineteen" };
+        public string[] waterhouse10 = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty",
+        "Seventy", "Eighty", "Ninety"};
+        public string WaterHouse100 = "Hundred";
+        public string WaterHouse1000 = "Thousand";
+        public string WaterHouseMillion = "Million";
+        public string Split = "-";
+
+        public string Chance(int data)
+        {
+            string UnionData = "";
+            if (data == 0)
+                return "Cero";
+            int Data_Moment;
+            while (data != 0 || data > 0)
+            {
+                // Caso para datos mayores que 19 y menores que 100
+                if(data > 19 && data < 100)
+                {
+                    Data_Moment = data / 10;
+                    UnionData += waterhouse10[Data_Moment] + " ";
+                    data = data % 10;
+                    
+                }
+                if(data > 99 && data < 1000)
+                {
+
+                }
+                if(data > 999999)
+                {
+
+                }
+                if (data < 20)
+                {
+                    UnionData += waterhouse[data] + " ";
+                    data -= data; 
+                }
+            }
+
+            return UnionData;
+
+           
+        }
+    }
+}
---
+++ CODE KATAS/3. Numbers in Words/NTest/Program.cs
@@ -0,0 +1,77 @@
+﻿using CodeKatas;
+using NUnit.Framework;
+
+namespace NTest
+{
+    [TestFixture]
+    class Program
+    {
+        [Test]
+        public void Cero()
+        {
+           string Expected = "Cero";
+            int data = 0;
+            Converted test1 = new Converted();
+           Assert.AreEqual(Expected, test1.Chance(data));  
+        }
+        [Test]
+        public void One()
+        {
+            string Expected = "One ";
+            int data = 1;
+            Converted test1 = new Converted();
+            Assert.AreEqual(Expected, test1.Chance(data));
+
+        }
+        [Test]
+        public void Two()
+        {
+            string Expected = "Two ";
+            int data = 2;
+            Converted test1 = new Converted();
+            Assert.AreEqual(Expected, test1.Chance(data));
+        }
+        [Test]
+        public void Ten()
+        {
+            string Expected = "Ten ";
+            int data = 10;
+            Converted test1 = new Converted();
+            Assert.AreEqual(Expected, test1.Chance(data));
+        }
+        [Test]
+        public void Eleven()
+        { 
+            string Expected = "Eleven ";
+            int data = 11;
+            Converted test1 = new Converted();
+            Assert.AreEqual(Expected, test1.Chance(data));
+        }
+        [Test]
+        public void Nineteen()
+        {
+            string Expected = "Nineteen ";
+            int data = 19;
+            Converted test1 = new Converted();
+            Assert.AreEqual(Expected, test1.Chance(data));
+        }
+        [Test]1
+        public void Ninety_Five()
+        {
+            string Expected = "Ninety Five ";
+            int data = 95;
+            Converted test1 = new Converted();
+            Assert.AreEqual(Expected, test1.Chance(data));
+        }
+        [Test]
+        public void Twenty_Four()
+        {
+            string Expected = "Twenty Four ";
+            int data = 24;
+            Converted test1 = new Converted();
+            Assert.AreEqual(Expected, test1.Chance(data));
+        }
+
+
+    }
+}
