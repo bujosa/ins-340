e81e076c6ae925f5c6238005e0731a300d34f308
bujosa <davidbujosa@gmail.com>
2019-06-09 20:24:11 -0400

    Nuevos cambios

---
+++ CODE KATAS/3. Numbers in Words/Part2/CodeKatas/Program.cs
@@ -0,0 +1,81 @@
+﻿
+namespace CodeKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+
+    public class Converted
+    {
+        string[] WaterHouse = {"", "One", "Two", "Three","Four","Five",
+        "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+        "Thirteen", "Fourteen", "Fiveteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        string[] WaterHouse20 = { "", "Ten", "Twenty", "Thirty", "Fouty", "Fifty",
+        "Sixty", "Seventy", "Eighty", "Ninety"};
+        string Hundred = "hundred";
+        string Thousand = "thousand";
+        string Million = "million";
+
+
+        public string Chance(int data)
+        {
+            string UnionData = "";
+            int DataMoment;
+            if (data == 0)
+                return "Cero";
+            while (data != 0 || data > 0)
+            {
+                if (data > 19 && data < 100)
+                {
+                    DataMoment = data / 10;
+                    UnionData += WaterHouse20[DataMoment] + " ";
+                    data = data % 10;
+                }
+                if (data < 20)
+                {
+                    UnionData += WaterHouse[data] + " ";
+                    data -= data;
+                }
+                if (data > 99 && data < 1000)
+                {
+                    DataMoment = data / 100;
+                    UnionData += WaterHouse[DataMoment] + " " + Hundred + " ";
+                    data = data % 100;
+                }
+                if(data > 999 && data < 1000000)
+                {
+                    DataMoment = data / 1000;
+                    int TMP = DataMoment = data / 1000;
+                    while (TMP != 0 || TMP > 0)
+                    {
+                        if (TMP > 19 && TMP < 100)
+                        {
+                            DataMoment = TMP / 10;
+                            UnionData += WaterHouse20[DataMoment] + " ";
+                            TMP = data % 10;
+                        }
+                        if (TMP < 20)
+                        {
+                            UnionData += WaterHouse[TMP] + " ";
+                            TMP -= TMP;
+                        }
+                        if (TMP > 99 && TMP < 1000)
+                        {
+                            DataMoment = TMP / 100;
+                            UnionData += WaterHouse[DataMoment] + " " + Hundred + " ";
+                            TMP = TMP % 100;
+                        }
+                    }
+                    UnionData += Thousand + " ";
+                    data = data % 1000;
+                }
+            }
+            return UnionData;
+        }
+
+    }
+}
---
+++ CODE KATAS/3. Numbers in Words/Part2/NTest/Program.cs
@@ -0,0 +1,114 @@
+﻿using CodeKatas;
+using NUnit.Framework;
+
+namespace NTest
+{
+    [TestFixture]
+    class Program
+    {
+        [Test]
+        public void Cero()
+        {
+            Converted Test1 = new Converted();
+            string expected = "Cero";
+            int data = 0;
+           Assert.AreEqual(expected, Test1.Chance(data));  
+        }
+
+        [Test]
+        public void One()
+        {
+            Converted Test1 = new Converted();
+            string expected = "One ";
+            int data = 1;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+
+        [Test]
+        public void Five()
+        {
+            Converted Test1 = new Converted();
+            string expected = "Five ";
+            int data = 5;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+        [Test]
+        public void Ten()
+        {
+            Converted Test1 = new Converted();
+            string expected = "Ten ";
+            int data = 10;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+        [Test]
+        public void Nineteen()
+        {
+            Converted Test1 = new Converted();
+            string expected = "Nineteen ";
+            int data = 19;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+        [Test]
+        public void Twenty_Eight()
+        {
+            Converted Test1 = new Converted();
+            string expected = "Twenty Eight ";
+            int data = 28;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+        [Test]
+        public void Fouty_Five()
+        {
+            Converted Test1 = new Converted();
+            string expected = "Fouty Five ";
+            int data = 45;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+        [Test]
+        public void Ninety_Nine()
+        {
+            Converted Test1 = new Converted();
+            string expected = "Ninety Nine ";
+            int data = 99;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+        [Test]
+        public void One_hundred()
+        {
+            Converted Test1 = new Converted();
+            string expected = "One hundred ";
+            int data = 100;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+        [Test]
+        public void Five_Hundred_Ninety_Nine()
+        {
+            Converted Test1 = new Converted();
+            string expected = "Five hundred Ninety Nine ";
+            int data = 599;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+        [Test]
+        public void One_thousand()
+        {
+            Converted Test1 = new Converted();
+            string expected = "One thousand ";
+            int data = 1000;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+        [Test]
+        public void Nine_hundred_Ninety_Nine_thousand_Nine_houndred_Ninety_Nine()
+        {
+            Converted Test1 = new Converted();
+            string expected = "Nine hundred Ninety Nine thousand Nine hundred Ninety Nine ";
+            int data = 999999;
+            Assert.AreEqual(expected, Test1.Chance(data));
+        }
+
+
+
+
+
+
+    }
+}
