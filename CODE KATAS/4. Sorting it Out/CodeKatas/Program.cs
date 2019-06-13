b6b217847a83c847542967266d2cc27cb993c227
bujosa <davidbujosa@gmail.com>
2019-06-12 20:27:56 -0400

    Sorting it out part 1

---
+++ CODE KATAS/4. Sorting it Out/CodeKatas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace CodeKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+
+    public class Sorting
+    {
+        public int[] Sorted(int[] B)
+        {
+
+            if (B.Length == 1)
+                return B;
+
+            for(int i = 0; i < B.Length - 1;i++)
+            {
+                bool Swat = false;
+                for(int j = 0; j < B.Length - 1; j++)
+                {
+                    if(B[j] > B[j + 1] )
+                    {
+                        int x = B[j];
+                        int Y = B[j + 1];
+                        B[j] = Y;
+                        B[j + 1] = x;
+                        Swat = true;
+                    }  
+                }
+                if (!Swat)
+                    break;
+            }
+            return B;
+        }
+    }
+}
---
+++ CODE KATAS/4. Sorting it Out/NTest/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using CodeKatas;
+using NUnit.Framework;
+
+namespace NTest
+{
+    [TestFixture]
+    class Program
+    {
+        [Test]
+        public void EmtyString()
+        {
+            int[] Expected = {  };
+            int[] Push = {  };
+            Sorting data = new Sorting();
+            Assert.AreEqual(Expected, data.Sorted(Push));
+        }
+        [Test]
+        public void OneSorted()
+        {
+            int[] Expected = {25 };
+            int[] Push = { 25};
+            Sorting data = new Sorting();
+            Assert.AreEqual(Expected, data.Sorted(Push));
+        }
+        [Test]
+        public void TwoSorted()
+        {
+            int[] Expected = { 25, 35 };
+            int[] Push = { 35, 25 };
+            Sorting data = new Sorting();
+            Assert.AreEqual(Expected, data.Sorted(Push));
+        }
+        [Test]
+        public void ThreeSorted()
+        {
+            int[] Expected = { 1, 25, 35 };
+            int[] Push = { 35, 25, 1 };
+            Sorting data = new Sorting();
+            Assert.AreEqual(Expected, data.Sorted(Push));
+        }
+        [Test]
+        public void TwentySorted()
+        {
+            int[] Expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int[] Push = { 20, 5, 1, 4, 3, 2, 7, 19, 6, 17, 16, 8, 10, 9, 11, 15, 13, 12, 14, 18 };
+            Sorting data = new Sorting();
+            Assert.AreEqual(Expected, data.Sorted(Push));
+        }
+        [Test]
+        public void Sorted()
+        {
+            int[] Expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int[] Push = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            Sorting data = new Sorting();
+            Assert.AreEqual(Expected, data.Sorted(Push));
+        }
+
+
+    }
+}
