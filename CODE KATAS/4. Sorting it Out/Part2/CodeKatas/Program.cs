bc392ebc5f01eeb44b5cb4aaed9da3a46e44e71d
bujosa <davidbujosa@gmail.com>
2019-06-14 21:41:46 -0400

    Part 2

---
+++ CODE KATAS/4. Sorting it Out/Part2/CodeKatas/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+namespace CodeKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+
+    public class Sorting
+    {
+        public int[] Sort(int[] push)
+        {
+            // Sort Bubble Sort
+            // Worst Case O(N^2) Average Same
+            // Best Case O(N)
+            for(int i = 0; i < push.Length - 1; i++)
+            {
+                bool Swat = false;
+                for(int j = 0; j < push.Length - 1; j++)
+                {
+                    if(push[j] > push[j+1])
+                    {
+                        int x = push[j];
+                        int y = push[j + 1];
+                        push[j] = y;
+                        push[j + 1] = x;
+                        Swat = true;
+                    }
+                }
+                if (!Swat)
+                    break;
+            }
+            return push;
+        }
+
+        public string Sort(string y)
+        {
+            int z = y.Length;
+            string house = "";
+            for(int i = 0; i < z; i++)
+            {
+                if (y.Substring(i, 1) != " " && y.Substring(i, 1) != "." && y.Substring(i, 1) != ",")
+                    house += y.Substring(i, 1);
+            }
+
+            string ordenado = new string(house.OrderBy(x => x).ToArray());
+
+            return ordenado;
+        }
+    }
+}
---
+++ CODE KATAS/4. Sorting it Out/Part2/NTest/Program.cs
@@ -0,0 +1,108 @@
+﻿
+using CodeKatas;
+using NUnit.Framework;
+
+namespace NTest
+{
+    [TestFixture]
+    class Program
+    {
+        [Test]
+        public void EmtyNumber()
+        {
+            int[] Expected = { };
+            int[] Push = { };
+
+            Sorting test = new Sorting();
+            Assert.AreEqual(Expected, test.Sort(Push));
+        }
+        [Test]
+        public void OneNumber()
+        {
+            int[] Expected = {25 };
+            int[] Push = { 25};
+
+            Sorting test = new Sorting();
+            Assert.AreEqual(Expected, test.Sort(Push));
+        }
+        [Test]
+        public void TwoNumbers()
+        {
+            int[] Expected = { 25, 35 };
+            int[] Push = { 35, 25 };
+
+            Sorting test = new Sorting();
+            Assert.AreEqual(Expected, test.Sort(Push));
+        }
+        [Test]
+        public void ThreeNumbers()
+        {
+            int[] Expected = { 25, 35, 45 };
+            int[] Push = { 35, 25, 45 };
+
+            Sorting test = new Sorting();
+            Assert.AreEqual(Expected, test.Sort(Push));
+        }
+
+        [Test]
+        public void FourtNumbers()
+        {
+            int[] Expected = { 1, 25, 35, 45 };
+            int[] Push = { 35, 25, 45, 1 };
+
+            Sorting test = new Sorting();
+            Assert.AreEqual(Expected, test.Sort(Push));
+        }
+
+
+        [Test]
+        public void TwentyNumbers()
+        {
+            int[] Expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ,16 ,17 ,18 ,19, 20 };
+            int[] Push = { 20, 10, 19, 9, 8, 18, 17, 7, 16, 6, 14, 13, 12, 11, 2, 3, 4, 5, 15, 1 };
+
+            Sorting test = new Sorting();
+            Assert.AreEqual(Expected, test.Sort(Push));
+        }
+
+        [Test]
+        public void TwentyNumbersSorted()
+        {
+            int[] Expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int[] Push = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            Sorting test = new Sorting();
+            Assert.AreEqual(Expected, test.Sort(Push));
+        }
+        [Test]
+        public void OneWords()
+        {
+            string Expected = "ABCDEF";
+            string Push = "FECDAB";
+            Sorting test = new Sorting();
+            Assert.AreEqual(Expected, test.Sort(Push));
+        }
+    
+    [Test]
+    public void OneWordsTwo()
+    {
+        string Expected = "ABCDEFGHI";
+        string Push = "FECDAB IGH";
+        Sorting test = new Sorting();
+        Assert.AreEqual(Expected, test.Sort(Push));
+    }
+    [Test]
+    public void TwoWords()
+    {
+            string Expected = "aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy";
+            string Push = "when not studying nuclear physics, bambi likes to play beach volleyball.";
+            Sorting test = new Sorting();
+            Assert.AreEqual(Expected, test.Sort(Push));
+    }
+
+
+
+
+
+    }
+}
