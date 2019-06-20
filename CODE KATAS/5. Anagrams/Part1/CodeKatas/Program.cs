103941cfab79025aa7cf22c4fc2cc10b174366e3
bujosa <davidbujosa@gmail.com>
2019-06-19 23:12:36 -0400

    Anagrams Part1

---
+++ CODE KATAS/5. Anagrams/Part1/CodeKatas/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace CodeKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+
+    public class Anagrams
+    {
+        public string Find(string Anagram, string words2)
+        {
+            if (words2 == null)
+                return null;
+
+            string TMP;
+            string Value = Anagram;
+            string[] words = words2.Split(" ");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                TMP = words[i];
+                if (TMP.Length > Anagram.Length || TMP.Length < Anagram.Length)
+                    continue; // Ahorraria el tiempo de entrar  a IsAnagram 
+                // y evaluar una palabra con menor tamaño o 
+                //  Mayor Tamaño que ese anagrama.
+
+                if (IsAnagram(Anagram, TMP))
+                    Value += " " + TMP;
+            }
+            
+            return Value;
+        }
+
+        public bool IsAnagram(string a1, string a2)
+        {
+            string a3 = Sort(a1);
+            string a4 = Sort(a2);
+
+            if (a3 == a4)
+                return true;
+            return false;
+        }
+
+        public string Sort(string y)
+        {
+            int z = y.Length;
+            string house = "";
+            for (int i = 0; i < z; i++)
+            {
+                if (y.Substring(i, 1) != " " && y.Substring(i, 1) != "." && y.Substring(i, 1) != ",")
+                    house += y.Substring(i, 1);
+            }
+
+            string ordenado = new string(house.OrderBy(x => x).ToArray());
+
+            return ordenado;
+        }
+    }
+}
---
+++ CODE KATAS/5. Anagrams/Part1/NTest/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using CodeKatas;
+using NUnit.Framework;
+
+namespace NTest
+{
+    [TestFixture]
+    class Program
+    {
+        [Test]
+        public void EmtyString()
+        {
+            string Anagram = null;
+            string Expected = null;
+            string Words = null;
+
+            Anagrams test1 = new Anagrams();
+
+            Assert.AreEqual(Expected, test1.Find(Anagram,Words));
+        
+        }
+        [Test]
+        public void OneWords()
+        {
+            string Expected = "hola olah";
+            string Anagram = "hola";  //  Anagrama
+            string Words = "olah"; //  Conjunto de palabras 
+
+            Anagrams test2 = new Anagrams();
+
+            Assert.AreEqual(Expected, test2.Find(Anagram,Words));
+
+        }
+        [Test]
+        public void TwoWords()
+        {
+            string Expected = "hola olah hlao";
+            string Anagram = "hola";  //  Anagrama
+            string Words = "olah hlao"; //  Conjunto de palabras 
+
+            Anagrams test2 = new Anagrams();
+
+            Assert.AreEqual(Expected, test2.Find(Anagram, Words));
+
+        }
+        [Test]
+        public void ThreeWords()
+        {
+            string Expected = "hola olah hlao";
+            string Anagram = "hola";  //  Anagrama
+            string Words = "olah hlao vizco"; //  Conjunto de palabras 
+
+            Anagrams test3 = new Anagrams();
+
+            Assert.AreEqual(Expected, test3.Find(Anagram, Words));
+
+        }
+        [Test]
+        public void FiveWords()
+        {
+            string Expected = "hola hlao";
+            string Anagram = "hola";  //  Anagrama
+            string Words = "camicasegrande barbaro budo hlao vizco"; //  Conjunto de palabras 
+
+            Anagrams test2 = new Anagrams();
+
+            Assert.AreEqual(Expected, test2.Find(Anagram, Words));
+
+        }
+        [Test]
+        public void TestWords()
+        {
+            string Expected = "crepitus cuprites pictures piecrust";
+            string Anagram = "crepitus";  //  Anagrama
+            string Words = "hamilton cuprites pictures piecrust"; //  Conjunto de palabras 
+
+            Anagrams test2 = new Anagrams();
+
+            Assert.AreEqual(Expected, test2.Find(Anagram, Words));
+
+        }
+
+
+
+    }
+}
