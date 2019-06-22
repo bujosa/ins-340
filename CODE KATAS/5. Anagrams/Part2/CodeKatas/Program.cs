6fb4b041634d4249931e571a25b56c3df845a1f3
bujosa <davidbujosa@gmail.com>
2019-06-21 22:09:58 -0400

    Anagram Part2

---
+++ CODE KATAS/5. Anagrams/Part2/CodeKatas/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace CodeKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+
+    public class Anagrams
+    {
+        // Funcion para encontrar anagramas
+        public string Find(string anagram, string words)
+        {
+            if (words == null || anagram == null) 
+              return null;
+            string value = anagram;
+            string[] Words = words.Split(" ");
+     
+           for (int i = 0; i < Words.Length; i++)
+           {
+                string TMP = Words[i];
+                if (IsAnagram(anagram, TMP))
+                    value += " " + TMP;
+            
+           }
+            return value;
+        }
+        // Funcion con el mismo nombre
+        public string Finder( string anagram, string rout)
+        {
+          
+            if (rout == null || anagram == null)
+                return null;
+            string value = anagram;
+            string[] Words = File.ReadAllLines(rout);
+
+            for (int i = 0; i < Words.Length; i++)
+            {
+                string TMP = Words[i];
+                if (anagram == TMP)
+                    continue;
+                if (IsAnagram(anagram, TMP))
+                    value += " " + TMP;
+
+            }
+            return value;
+        }
+        // Funcion para saber si dos palabras son anagramas
+        private bool IsAnagram(string anagram, string tMP)
+        {
+            string WordsOne = Sort(anagram);
+            string wordstwo = Sort(tMP);
+            
+            if (WordsOne == wordstwo)
+                return true;
+            return false;
+        }
+
+        // Funcion para ordernar palabras like sort
+        public string Sort(string y)
+        {
+            int z = y.Length;
+            string house = "";
+            for (int i = 0; i < z; i++)
+            {
+                if (y.Substring(i, 1) != " " && y.Substring(i, 1) != "." && y.Substring(i, 1) != ",")
+                    house += y.Substring(i, 1);
+            }
+
+            string ordenado = new string(house.OrderBy(x => x).ToArray());
+
+            return ordenado;
+        }
+    }
+}
---
+++ CODE KATAS/5. Anagrams/Part2/NTest/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using CodeKatas;
+using NUnit.Framework;
+
+namespace NTest
+{
+    [TestFixture]
+    class Program
+    {
+        [Test]
+        public void EmptyString()
+        {
+            string Anagram = null;
+            string Expected = null;
+            string Words = null;
+
+            Anagrams test1 = new Anagrams();
+
+            Assert.AreEqual(Expected, test1.Find(Anagram, Words));
+        }
+        [Test]
+        public void OneWords()
+        {
+            string Anagram ="mash";
+            string Expected = "mash hasm";
+            string Words = "hasm";
+
+            Anagrams test2 = new Anagrams();
+
+            Assert.AreEqual(Expected, test2.Find(Anagram, Words));
+        }
+        
+        [Test]
+        public void TwoWords()
+        {
+            string Anagram = "kinship";
+            string Expected = "kinship pinkish";
+            string rout = @"C:\Users\David\Desktop\Anagrams.txt";
+            Anagrams test3 = new Anagrams();
+
+            Assert.AreEqual(Expected, test3.Finder(Anagram, rout));
+        }
+        [Test]
+        public void SixWords_in_Rout()
+        {
+            string Anagram = "enlist";
+            string Expected = "enlist elints inlets listen silent tinsel";
+            string rout = @"C:\Users\David\Desktop\Anagrams.txt";
+            Anagrams test4 = new Anagrams();
+
+            Assert.AreEqual(Expected, test4.Finder(Anagram, rout));
+        }
+        [Test]
+        public void ThreeWords_in_Rout()
+        {
+            string Anagram = "boaster";
+            string Expected = "boaster boaters borates rebatos sorbate";
+            string rout = @"C:\Users\David\Desktop\Anagrams.txt";
+            Anagrams test5 = new Anagrams();
+
+            Assert.AreEqual(Expected, test5.Finder(Anagram, rout));
+        }
+
+
+
+
+    }
+}
