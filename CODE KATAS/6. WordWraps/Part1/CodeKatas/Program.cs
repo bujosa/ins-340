75741274ffe6a42017b61da1a71cb39f53e270f8
bujosa <davidbujosa@gmail.com>
2019-06-26 01:37:35 -0400

    Word Wraps Part 1

---
+++ CODE KATAS/6. WordWraps/Part1/CodeKatas/Program.cs
@@ -0,0 +1,81 @@
+﻿
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace CodeKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wrapper dato = new Wrapper();
+            string prueba  = dato.WrapWords("hola", 2);
+        }
+        
+    }
+
+    public class Wrapper
+    {
+        public string WrapWords(string push, int x)
+        {
+            if (push.Length <= x)
+                return push;
+            else
+            {
+                char[] func = push.ToCharArray();
+                string global = "";
+                int count = 0;
+                for (int i = 0; i < func.Length; i++)
+                {
+                    if (func[i] == ' ')
+                    {
+                        global += func[i];
+                        count = 0;
+                        continue;
+                    }
+                    if (count < x)
+                    { global += func[i];
+                        count++;
+                    }
+                    else
+                    {
+                        count = 0;
+                        global += " " + func[i];
+                    }
+                    
+                }
+                return global;
+            }
+        }
+        public string WrapLines(string push, int x)
+        {
+            char[] func = push.ToCharArray();
+            string global = "";
+            int count = 0;
+
+            for (int i = 0; i < func.Length; i++)
+            {
+                if (func[i] == ' ')
+                {
+                    
+                    count++;
+                    if (count == x)
+                    {
+                        global += "\n ";
+                        count = 0;
+                    }
+                    else
+                        global += " ";
+                    continue;
+                }
+                global += func[i];
+            }
+               return global;
+        }
+        
+    }
+
+
+}
---
+++ CODE KATAS/6. WordWraps/Part1/NTest/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using CodeKatas;
+using NUnit.Framework;
+
+namespace NTest
+{
+    [TestFixture]
+    class Program
+    {
+        [Test]
+        public void OneWords()
+        {
+            Wrapper test1 = new Wrapper();
+            string expected = "ho la";
+            string Push = "hola";
+            int x = 2;
+            Assert.AreEqual(expected, test1.WrapWords(Push, x));
+        }
+        [Test]
+        public void ThreeWords()
+        {
+            Wrapper test1 = new Wrapper();
+            string expected = "hola vict or quer ido";
+            string Push = "hola victor querido";
+            int x = 4; // Representa maximo de letras unidas a una palabra 
+            Assert.AreEqual(expected, test1.WrapWords(Push, x));
+        }
+        [Test]
+        public void SeparetedLines()
+        {
+            Wrapper test1 = new Wrapper();
+            string expected = "hola victor querido amigo\n mio.";
+            string Push = "hola victor querido amigo mio.";
+            int x = 4; // Columna en una linea de texto maximo.
+            Assert.AreEqual(expected, test1.WrapLines(Push, x));
+        }
+
+
+
+    }
+}
