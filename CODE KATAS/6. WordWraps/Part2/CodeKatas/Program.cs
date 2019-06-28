38866cd4b1391162e7830a491040842fc4ad2e1f
bujosa <davidbujosa@gmail.com>
2019-06-28 02:22:25 -0400

    Word Wraps Part 2

---
+++ CODE KATAS/6. WordWraps/Part2/CodeKatas/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace CodeKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        }
+        
+    }
+
+    public class Wrapper
+    {
+        public string WrapWords(string push, int x)
+        {
+            char[] data = push.ToCharArray();
+            string global = "";
+            int count = 0;
+
+            if (push.Length < x)
+                return push;
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                if(data[i] == ' ')
+                {
+                    count = 0;
+                    global += " ";
+                    continue;
+                }
+                if (count < x)
+                {
+                    global += data[i];
+                    count++;
+                }
+                else
+                { global += " " + data[i];
+                    count = 1;
+                }
+
+            }
+            return global;
+        }
+
+        public string WrapLines(string push, int x)
+        {
+            char[] data = push.ToCharArray();
+            string global = "";
+            int count = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == ' ')
+                {
+                    count++;
+                    if(count == x)
+                    {
+                        count = 0;
+                        global += "\n ";
+
+                    }
+                    else
+                        global += " ";
+                    continue;
+                }
+                global += data[i];
+            }
+            return global;
+        }
+    }
+
+
+}
---
+++ CODE KATAS/6. WordWraps/Part2/NTest/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using CodeKatas;
+using NUnit.Framework;
+
+namespace NTest
+{
+    [TestFixture]
+    class Program
+    {
+        [Test]
+        public void OneWords()
+        {
+
+            Wrapper test1 = new Wrapper();
+            string expected = "Pala bra";
+            string push = "Palabra";
+            int x = 4;
+            Assert.AreEqual(expected, test1.WrapWords(push, x));
+        }
+        [Test]
+        public void OneWordsLess()
+        {
+
+            Wrapper test1 = new Wrapper();
+            string expected = "Palabra";
+            string push = "Palabra";
+            int x = 8;
+            Assert.AreEqual(expected, test1.WrapWords(push, x));
+        }
+        [Test]
+        public void TwoWords()
+        {
+
+            Wrapper test1 = new Wrapper();
+            string expected = "Pal abr a Lov es";
+            string push = "Palabra Loves";
+            int x = 3; // Cantidad de letras por palabra
+            Assert.AreEqual(expected, test1.WrapWords(push, x));
+        }
+        [Test]
+        public void JumpLines()
+        {
+
+            Wrapper test1 = new Wrapper();
+            string expected = "Palabra Loves dios\n padre";
+            string push = "Palabra Loves dios padre";
+            int x = 3; // Cantidad de columna en una linea
+            Assert.AreEqual(expected, test1.WrapLines(push, x));
+        }
+        [Test]
+        public void JumpTwoLines()
+        {
+
+            Wrapper test1 = new Wrapper();
+            string expected = "Palabra Loves\n dios padre\n amado";
+            string push = "Palabra Loves dios padre amado";
+            int x = 2; // Cantidad de columna en una linea
+            Assert.AreEqual(expected, test1.WrapLines(push, x));
+        }
+
+
+
+    }
+}
